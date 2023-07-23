021dbbb91718fa303f45a63d0101ae8e9db1cb14
tharupathi02 <109888408+tharupathi02@users.noreply.github.com>
2023-07-23 12:04:08 +0530

    Add booking request accept or reject function

--- app/src/main/java/com/leoxtech/garageapp/Screens/BookingDetails.kt
+++ app/src/main/java/com/leoxtech/garageapp/Screens/BookingDetails.kt
@@ -3,9 +3,13 @@ package com.leoxtech.garageapp.Screens
 import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.RatingBar
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -28,6 +32,9 @@ class BookingDetails : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var dialog: AlertDialog
 
+    val bookingRequestImages = ArrayList<String>()
+    private var bookingId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookingDetailsBinding.inflate(layoutInflater)
@@ -37,13 +44,124 @@ class BookingDetails : AppCompatActivity() {
 
         dialogBox()
 
-        val bookingId = intent.getStringExtra("key")
+        bookingId = intent.getStringExtra("key")
         getBookingDetails(bookingId!!)
 
+        clickListeners()
+
+    }
+
+    private fun clickListeners() {
         binding.cardBack.setOnClickListener {
             finish()
         }
 
+        binding.btnAccept.setOnClickListener {
+            acceptBooking()
+        }
+
+        binding.btnReject.setOnClickListener {
+            rejectBooking()
+
+        }
+    }
+
+    private fun acceptBooking() {
+        dialog.show()
+        val builder = MaterialAlertDialogBuilder(this)
+        val view = LayoutInflater.from(this).inflate(R.layout.custom_dialog_booking, null)
+        val txtNote = view.findViewById<TextInputLayout>(R.id.txtNote)
+
+        builder.setPositiveButton("Submit") { dialogBox, which ->
+            if (txtNote.editText!!.text.toString().isEmpty()) {
+                txtNote.error = "Please Enter a Note"
+                txtNote.requestFocus()
+            } else {
+                txtNote.error = null
+                dbRef = FirebaseDatabase.getInstance().getReference(Common.BOOKING_REF)
+                dbRef.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()) {
+                            for (bookingSnapshot in snapshot.children) {
+                                if (bookingSnapshot.child("key").value.toString() == bookingId) {
+                                    bookingSnapshot.child("bookingStatus").ref.setValue("Accepted")
+                                    bookingSnapshot.child("bookingNote").ref.setValue(txtNote.editText!!.text.toString())
+                                    dialog.dismiss()
+                                    finish()
+                                }
+                            }
+
+                        } else {
+                            dialog.dismiss()
+                            Toast.makeText(this@BookingDetails, "Accepting Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        dialog.dismiss()
+                        Toast.makeText(this@BookingDetails, error.message, Toast.LENGTH_SHORT).show()
+                    }
+                })
+                dialogBox.dismiss()
+            }
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        builder.setView(view)
+        val confirmationDialog = builder.create()
+        confirmationDialog.show()
+    }
+
+    private fun rejectBooking() {
+        dialog.show()
+        val builder = MaterialAlertDialogBuilder(this)
+        val view = LayoutInflater.from(this).inflate(R.layout.custom_dialog_booking, null)
+        val txtNote = view.findViewById<TextInputLayout>(R.id.txtNote)
+
+        builder.setPositiveButton("Submit") { dialogBox, which ->
+            if (txtNote.editText!!.text.toString().isEmpty()) {
+                txtNote.error = "Please Enter a Note"
+                txtNote.requestFocus()
+            } else {
+                txtNote.error = null
+                dbRef = FirebaseDatabase.getInstance().getReference(Common.BOOKING_REF)
+                dbRef.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()) {
+                            for (bookingSnapshot in snapshot.children) {
+                                if (bookingSnapshot.child("key").value.toString() == bookingId) {
+                                    bookingSnapshot.child("bookingStatus").ref.setValue("Rejected")
+                                    bookingSnapshot.child("bookingNote").ref.setValue(txtNote.editText!!.text.toString())
+                                    dialog.dismiss()
+                                    finish()
+                                }
+                            }
+
+                        } else {
+                            dialog.dismiss()
+                            Toast.makeText(this@BookingDetails, "Accepting Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        dialog.dismiss()
+                        Toast.makeText(this@BookingDetails, error.message, Toast.LENGTH_SHORT).show()
+                    }
+                })
+                dialogBox.dismiss()
+            }
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        builder.setView(view)
+        val confirmationDialog = builder.create()
+        confirmationDialog.show()
     }
 
     private fun getBookingDetails(bookingKey: String) {
@@ -71,13 +189,17 @@ class BookingDetails : AppCompatActivity() {
                                 binding.layoutCustomerAddress.visibility = View.GONE
                             }
 
-                            val bookingRequestImages = ArrayList<String>()
                             for (imageSnapshot in bookingSnapshot.child("imageList").children) {
                                 bookingRequestImages.add(imageSnapshot.value.toString())
                             }
                             binding.recyclerBookingImages.adapter = ImageLargeAdapter(this@BookingDetails, bookingRequestImages)
                             binding.recyclerBookingImages.layoutManager = LinearLayoutManager(this@BookingDetails, LinearLayoutManager.HORIZONTAL, false)
                             binding.recyclerBookingImages.setHasFixedSize(true)
+
+                            if (bookingSnapshot.child("bookingStatus").value.toString() == "Rejected" || bookingSnapshot.child("bookingStatus").value.toString() == "Accepted") {
+                                binding.btnAccept.visibility = View.GONE
+                                binding.btnReject.visibility = View.GONE
+                            }
 
                             dialog.dismiss()
                             getCustomerDetails(bookingSnapshot.child("customerId").value.toString())
@@ -98,7 +220,6 @@ class BookingDetails : AppCompatActivity() {
     }
 
     private fun getCustomerDetails(customerId: String) {
-        dialog.show()
         dbRef = FirebaseDatabase.getInstance().getReference(Common.USER_REFERENCE)
         dbRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -107,18 +228,15 @@ class BookingDetails : AppCompatActivity() {
                         if (customerSnapshot.child("uid").value.toString() == customerId) {
                             binding.txtCustomerName.text = customerSnapshot.child("name").value.toString()
                             binding.txtMobileNumber.text = customerSnapshot.child("phone").value.toString()
-                            dialog.dismiss()
                         }
                     }
 
                 } else {
-                    dialog.dismiss()
-                    Toast.makeText(this@BookingDetails, "No Bookings", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@BookingDetails, "No Customer Details Found", Toast.LENGTH_SHORT).show()
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
-                dialog.dismiss()
                 Toast.makeText(this@BookingDetails, error.message, Toast.LENGTH_SHORT).show()
             }
         })
